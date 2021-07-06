3311688be3f948052bc1152a3ec08ec6b8bc8ddb
IuliiaKalenyk <yulyaa.kalenyk@gmail.com>
2021-07-06 21:26:06 +0300

    components добавила

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+import Section from './components/Section/Section';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+  filter: ''
+}
+  render() {
+    return (
+      <div>
+  <Section title="Phonebook">
+  <ContactForm/>
+</Section>
+  <Section title="Contacts">
+  <Filter/>
+  <ContactList />
+  </Section>
+</div>
+    );
+  }
 }
 
 export default App;
